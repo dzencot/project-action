afc56c649c5e921e22bd374711668d670a799079
plugin73 <laplugin73@gmail.com>
2020-09-04 11:23:33 +0000

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 mkdir -p /tmp/builds/source
 
+docker run -v "/tmp/builds:/tmp/builds" $1:release bash -c 'ls -la /tmp/builds'
 docker run -v "/tmp/builds:/tmp/builds" $1:release bash -c 'cp -r /project/* /tmp/builds/source && rm -rf /tmp/builds/source/code'
 
 mkdir -p /tmp/builds/source/code
