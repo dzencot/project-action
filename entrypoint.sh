a852ed24e544b8f0a51923a2c4ff9c1009fbe35c
plugin73 <laplugin73@gmail.com>
2020-09-04 13:58:51 +0000

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@
 echo "mkdir"
 mkdir -p /tmp/builds/source
 
-echo "docker run"
-
-echo $(docker version)
+echo $GITHUB_WORKSPACE
+echo "$(ls -la $GITHUB_WORKSPACE)"
 
 docker run -v "/tmp/builds/source:/tmp/builds/source:rw" $1:release bash -c 'cp -r /project/* /tmp/builds/source && ls -la /tmp/builds/source && rm -rf /tmp/builds/source/code'
 
