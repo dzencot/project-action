d23c9b77423ebcdef1a90f8b5f97ebffbb8a83c9
plugin73 <laplugin73@gmail.com>
2020-09-04 08:57:00 +0000

    custom entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+mkdir -p /tmp/builds/source
 
+docker run -v /tmp/builds:/mnt $1:release bash -c 'cp -r /project/* /mnt/source && rm -rf /mnt/source/code'
+
+mkdir -p /tmp/builds/source/code
+cp -r `pwd`/* /tmp/builds/source/code
+cd /tmp/builds/source && docker-compose run development make setup test lint
