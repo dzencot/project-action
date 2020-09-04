95c4a88898134c9b26350f19718d414ee15d182a
plugin73 <laplugin73@gmail.com>
2020-09-04 11:43:11 +0000

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,8 @@ mkdir -p /tmp/builds/source
 
 ls -la  /tmp/builds
 
-docker run -v "/tmp/builds/:/tmp/builds/" $1:release bash -c 'ls -la /tmp'
-docker run -v "/tmp/builds:/tmp/builds" $1:release bash -c 'cp -r /project/* /tmp/builds/source && rm -rf /tmp/builds/source/code'
+docker run -v "/tmp/builds/source:/tmp/builds/source" $1:release bash -c 'cp -r /project/* /tmp/builds/source && rm -rf /tmp/builds/source/code'
 
 mkdir -p /tmp/builds/source/code
-cp -r `pwd`/* /tmp/builds/source/code
+cp -r $(pwd)/* /tmp/builds/source/code
 cd /tmp/builds/source && docker-compose run development make setup test lint
