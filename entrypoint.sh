5afba9318ed8129e531968d31ae7de2cee9c2da0
plugin73 <laplugin73@gmail.com>
2020-09-04 11:24:51 +0000

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 mkdir -p /tmp/builds/source
 
+ls -la  /tmp
+
 docker run -v "/tmp/builds:/tmp/builds" $1:release bash -c 'ls -la /tmp/builds'
 docker run -v "/tmp/builds:/tmp/builds" $1:release bash -c 'cp -r /project/* /tmp/builds/source && rm -rf /tmp/builds/source/code'
 
