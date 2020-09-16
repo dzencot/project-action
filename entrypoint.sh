625166ddf92bb6426425dd81cd8ab6d8a1978412
plugin73 <laplugin73@gmail.com>
2020-09-16 13:54:39 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh -l
+export BUILD_DIRECTORY $(pwd)/build
+
+mkdir $BUILD_DIRECTORY
 
 docker run --name project-source  $1:release bash -c 'ls -la /project/'
-docker cp  project-source:/project /tmp
+docker cp  project-source:/project $BUILD_DIRECTORY
 
-rm -rf /tmp/project/code/*
+rm -rf $(BUILD_DIRECTORY)/code/*
 
-ls -la $(pwd)
+cp -r $(pwd)/* $(BUILD_DIRECTORY)/code/
 
-cp -r $(pwd)/* /tmp/project/code/
+cat $(BUILD_DIRECTORY)/Makefile
 
-cat /tmp/project/Makefile
+cd $(BUILD_DIRECTORY)/project && docker-compose run development cat Makefile
+# cd /tmp/project && docker-compose run development ls -la /project
 
-cd /tmp/project && docker-compose run development cat Makefile
-cd /tmp/project && docker-compose run development ls -la /project
-
-docker ps -a
-docker inspect $(docker ps -a | grep 'project_development' | awk '{print $1}')
+# docker ps -a
+# docker inspect $(docker ps -a | grep 'project_development' | awk '{print $1}')
 
 # cd /tmp/project && docker-compose run development make setup test lint
