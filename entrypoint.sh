f590959b645834be264b7f34deb1d149e9309417
plugin73 <laplugin73@gmail.com>
2020-09-16 12:13:08 +0000

    run linter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,10 @@
 #!/bin/sh -l
 
-echo "mkdir"
-mkdir -p /tmp/builds/source
-
-echo "ls -la /project/"
-
 docker run --name project-source  $1:release bash -c 'ls -la /project/'
-echo "docker cp"
 docker cp  project-source:/project /tmp
 
-echo "ls -la /tmp/project"
-ls -la /tmp/project
+rm -rf /tmp/project/code/*
 
-# mkdir -p /tmp/builds/source/code
-# cp -r $(pwd)/* /tmp/builds/source/code
+cp -r $(pwd)/* /tmp/project/code/
 
-# ls -la /tmp/builds/source
-
-# ls -la $(pwd)
-
-# cd /tmp/builds/source && docker-compose run development make setup test lint
+cd /tmp/project && docker-compose run development make setup test lint
