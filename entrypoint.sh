bd027fd03334553dc9cede14edd2b7b7aa80b7fa
plugin73 <laplugin73@gmail.com>
2020-09-16 14:17:21 +0000

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ mkdir $BUILD_DIRECTORY
 docker run --name project-source  $1:release bash -c 'ls -la /project/'
 docker cp  project-source:/project $BUILD_DIRECTORY
 
-rm -rf $(BUILD_DIRECTORY)/code/*
+rm -rf $BUILD_DIRECTORY/code/*
 
-cp -r $(pwd)/* $(BUILD_DIRECTORY)/code/
+cp -r $(pwd)/* $BUILD_DIRECTORY/code/
 
-cat $(BUILD_DIRECTORY)/Makefile
+cat $BUILD_DIRECTORY/Makefile
 
-cd $(BUILD_DIRECTORY)/project && docker-compose run development cat Makefile
+cd $BUILD_DIRECTORY/project && docker-compose run development cat Makefile
 # cd /tmp/project && docker-compose run development ls -la /project
 
 # docker ps -a
