74571865a130b5cc5723b4aaa642ac15f1daa7ff
plugin73 <laplugin73@gmail.com>
2020-09-17 07:52:26 +0000

    ls /var/tmp/project-action/build

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,12 @@ export BUILD_DIRECTORY=$(pwd)/build
 echo "BUILD_DIRECTORY"
 echo $BUILD_DIRECTORY
 
-mkdir $BUILD_DIRECTORY/code
-
 docker run --name project-source  $1:release bash -c 'ls -la /project/'
 docker cp  project-source:/project $BUILD_DIRECTORY
+
+echo  "ls -la"
+
+ls -la $BUILD_DIRECTORY
 
 rm -rf $BUILD_DIRECTORY/code/*
 
