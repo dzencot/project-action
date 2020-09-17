2a5c9e7d2ea2833efa6f6583f391a1afe75c1d87
plugin73 <laplugin73@gmail.com>
2020-09-17 09:00:17 +0000

    show files inside docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,18 @@ export BUILD_DIRECTORY=$(pwd)/build
 echo "BUILD_DIRECTORY"
 echo $BUILD_DIRECTORY
 
-ls -la $BUILD_DIRECTORY
-
 docker run --name project-source  $1:release bash -c 'ls -la /project/'
 docker cp  project-source:/project $BUILD_DIRECTORY
-
-echo  "ls -la"
-
-ls -la $BUILD_DIRECTORY
 
 rm -rf $BUILD_DIRECTORY/code/*
 
 cp -r $(ls | grep -v '^build$') $BUILD_DIRECTORY/code/
 
+echo  "ls -la"
+ls -la $BUILD_DIRECTORY/code
+
 cd $BUILD_DIRECTORY && docker-compose run development cat Makefile
-# cd /tmp/project && docker-compose run development ls -la /project
+cd $BUILD_DIRECTORY && docker-compose run development ls -la
 
 # docker ps -a
 # docker inspect $(docker ps -a | grep 'project_development' | awk '{print $1}')
