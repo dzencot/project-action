3c5dfb37d7aed0e4f536352ce30f551cb8d8ecde
mokevnin <mokevnin@gmail.com>
2020-12-23 17:41:27 -0500

    fix run

--- src/bin/run.js
+++ src/bin/run.js
@@ -2,6 +2,8 @@ const path = require('path');
 const core = require('@actions/core');
 const run = require('../index.js');
 
+process.on('unhandledRejection', (up) => { throw up });
+
 const mountPoint = path.join('/', 'var', 'tmp');
 const verbose = core.getInput('verbose');
 const projectMemberId = core.getInput('project-member-id');
