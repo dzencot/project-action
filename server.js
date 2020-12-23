95f33302c3edab0ced3ec1bf48d2e88281e0b612
mokevnin <mokevnin@gmail.com>
2020-12-23 18:19:23 -0500

    add proxy

---
+++ server.js
@@ -0,0 +1,26 @@
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/api/user_project_github_workflow/project_members/:id', async (request, reply) => {
+  const result = {
+    tests_on: true,
+    project: {
+      image_name: 'hexlet-project-source-ci',
+    },
+  };
+
+  return result;
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
