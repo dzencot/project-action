88b7a649bbd5c83e5d161cb81ad796eed76d4298
mokevnin <mokevnin@gmail.com>
2020-12-23 18:06:16 -0500

    fix name

--- src/bin/run.js
+++ src/bin/run.js
@@ -4,13 +4,13 @@ const run = require('../index.js');
 
 process.on('unhandledRejection', (up) => { throw up });
 
-const mountPoint = path.join('/', 'var', 'tmp');
-const verbose = core.getInput('verbose', { required: true });
+const mountPath = path.join('/', 'var', 'tmp');
+const verbose = core.getInput('verbose', { required: true }) === 'true';
 const projectMemberId = core.getInput('project-member-id', { required: true });
 const projectPath = process.cwd();
 
 const options = {
-  projectPath, mountPoint, verbose, projectMemberId,
+  projectPath, mountPath, verbose, projectMemberId,
 };
 
 core.info(JSON.stringify(options));
