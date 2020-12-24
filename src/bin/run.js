8949307d4a7fa1aa2a8f60f117a85c64891c8892
plugin73 <laplugin73@gmail.com>
2020-12-24 09:34:51 +0000

    get HEXLET_ID from secrets

--- src/bin/run.js
+++ src/bin/run.js
@@ -8,7 +8,7 @@ process.on('unhandledRejection', (up) => { throw up; });
 
 const mountPath = path.join('/', 'var', 'tmp');
 const verbose = core.getInput('verbose', { required: true }) === 'true';
-const projectMemberId = core.getInput('hexlet-id', { required: true });
+const projectMemberId = process.env.HEXLET_ID;
 const projectPath = path.resolve(process.cwd(), process.env.ACTION_PROJECT_PATH || '');
 
 const options = {
