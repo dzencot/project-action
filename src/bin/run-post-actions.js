f563537d708f4ff7d67626052ecebb9e6e619487
plugin73 <laplugin73@gmail.com>
2020-12-29 14:05:59 +0000

    pass projectMemberId

--- src/bin/run-post-actions.js
+++ src/bin/run-post-actions.js
@@ -6,9 +6,10 @@ const { runPostActions } = require('../index.js');
 process.on('unhandledRejection', (up) => { throw up; });
 
 const mountPath = core.getInput('mount-path', { required: true });
+const projectMemberId = core.getInput('hexlet-id', { required: true });
 
 const options = {
-  mountPath,
+  mountPath, projectMemberId,
 };
 
 runPostActions(options);
