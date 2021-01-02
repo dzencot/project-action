cefde2b600862553d43a5b50606c62f191932be2
mokevnin <mokevnin@gmail.com>
2021-01-02 10:09:05 -0500

    improve routes

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,20 @@ const path = require('path');
 
 const apiUrl = '/api/user_project_github_workflow/';
 
+const buildUrl = (part, host) => {
+  const urlPath = path.join(apiUrl, part);
+  const url = new URL(urlPath, host);
+  return url.toString();
+};
+
 const buildRoutes = (host = 'https://hexlet.io') => ({
   projectMemberPath: (id) => {
-    const urlPath = path.join(apiUrl, `project_members/${id}.json`);
-    const url = new URL(urlPath, host);
-    return url.toString();
+    const url = buildUrl(`project_members/${id}.json`, host);
+    return url;
   },
   projectMemberCheckPath: (memberId) => {
-    const urlPath = path.join(apiUrl, `project_members/${memberId}/checks.json`);
-    const url = new URL(urlPath, host);
-    return url.toString();
+    const url = buildUrl(`project_members/${memberId}/checks.json`, host);
+    return url;
   },
 });
 
