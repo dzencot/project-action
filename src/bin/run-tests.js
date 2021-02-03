4c18796f7a1645ad25fdcf7d7b525ac19203221c
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-02-03 21:24:52 +0200

    fix source project

--- src/bin/run-tests.js
+++ src/bin/run-tests.js
@@ -9,13 +9,10 @@ process.on('unhandledRejection', (up) => { throw up; });
 core.exportVariable('COMPOSE_DOCKER_CLI_BUILD', 1);
 core.exportVariable('DOCKER_BUILDKIT', 1);
 
-// const mountPath = core.getInput('mount-path', { required: true });
-// const verbose = core.getInput('verbose', { required: true }) === 'true';
-// const projectMemberId = core.getInput('hexlet-id', { required: true });
+const mountPath = core.getInput('mount-path', { required: true });
+const verbose = core.getInput('verbose', { required: true }) === 'true';
+const projectMemberId = core.getInput('hexlet-id', { required: true });
 const projectPath = path.resolve(process.cwd(), process.env.ACTION_PROJECT_PATH || '');
-const mountPath = '/var/tmp';
-const verbose = true;
-const projectMemberId = 12438;
 
 const options = {
   projectPath, mountPath, verbose, projectMemberId,
